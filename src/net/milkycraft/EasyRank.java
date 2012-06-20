194f1eb22936e16380c3bbde78f27a176ce338d6
Nick Porillo <milkywayz@mail.com>
2012-06-19 22:31:44 -0400

    Added hooks, removed arraylists..

--- src/net/milkycraft/EasyRank.java
+++ src/net/milkycraft/EasyRank.java
@@ -3,20 +3,31 @@ package net.milkycraft;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import net.milkbowl.vault.permission.Permission;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import de.bananaco.bpermissions.api.ApiLayer;
+
+import ru.tehkode.permissions.bukkit.PermissionsEx;
+
+/**
+ * The Class EasyRank.
+ */
 public class EasyRank extends JavaPlugin{
+
 	public static Permission perms = null;
 	public static EasyRank main;
 	public static String maindirectory;
 	private ErCommandExecutor ce;
-	public static ArrayList<String> rank;
-	public static ArrayList<String> name;	
+	protected static PermissionsEx pex;
+	protected static ApiLayer bperms;
+	/* (non-Javadoc)
+	 * @see org.bukkit.plugin.java.JavaPlugin#onEnable()
+	 */
 	@Override
 	public void onEnable() {
 		main = this;
@@ -26,8 +37,6 @@ public class EasyRank extends JavaPlugin{
 		config.load();
 		Metrics metrics = null;
 		maindirectory = getDataFolder().getPath() + File.separator;
-		getServer().getPluginManager().registerEvents(new Ranker(),
-				this);
 		try {
 			metrics = new Metrics(main);
 		} catch (IOException e) {
@@ -37,31 +46,89 @@ public class EasyRank extends JavaPlugin{
 		writeInfo("[EasyRank] Successfully enabled!");
 	}
 
+	/* (non-Javadoc)
+	 * @see org.bukkit.plugin.java.JavaPlugin#onDisable()
+	 */
 	@Override
 	public void onDisable() {
 		writeInfo("[EasyRank] Successfully disabled!");
 	}
 	
+	/**
+	 * Sets up the executors.
+	 */
 	private void setUpExecutors() {
 		ce = new ErCommandExecutor(this);
 		getCommand("rank").setExecutor(ce);
 	}
 	
-	 private boolean setUpPermissions() {
+	 /**
+ 	 * Sets up the permissions.
+ 	 *
+ 	 * @return true, if successful
+ 	 */
+ 	private boolean setUpPermissions() {
 	        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
 	        perms = rsp.getProvider();
 	        return perms != null;
 	    }
+ 	
+ 	protected void setUpDependencies() {
+ 		try {
+			setupPex();
+		} catch (Exception e) {
+			writeWarn("Failed to load PermissionsEx.");
+			e.printStackTrace();
+		}
+		try {
+			setupBperms();
+		} catch (Exception e) {
+			writeWarn("Failed to load bPermissions.");
+			e.printStackTrace();
+		}
+ 	}
+ 	protected void setupPex() {
+ 		final Plugin pex = getServer().getPluginManager().getPlugin("PermissionsEx");
+ 		if(pex == null) {
+ 		} else {
+ 			EasyRank.pex = (PermissionsEx) pex;
+ 			writeInfo("[EasyRank] Sucessfully hooked into PermissionsEx");
+ 		}
+ 	}
+ 	protected void setupBperms() {
+ 		final Plugin bperms = getServer().getPluginManager().getPlugin("bPermissions");
+ 		if(bperms == null) {		
+ 		} else {
+ 			EasyRank.bperms = (ApiLayer) bperms;
+ 			writeInfo("[EasyRank] Sucessfully hooked into bPermissions");
+ 		}
+ 		
+ 	}
 	 
-	 public static void writeInfo(String info) {
+	 /**
+ 	 * Write info to console .
+ 	 *
+ 	 * @param info the info
+ 	 */
+ 	public static void writeInfo(String info) {
 		 java.util.logging.Logger.getLogger("Minecraft").info(info);
 	 }
 	 
-	 public static void writeWarn(String warn) {
+	 /**
+ 	 * Write warning to console .
+ 	 *
+ 	 * @param warn the warn
+ 	 */
+ 	public static void writeWarn(String warn) {
 		 java.util.logging.Logger.getLogger("Minecraft").warning(warn);
 	 }
 	 
-	 public static final EasyRank getMain() {
+	 /**
+ 	 * Gets the main class.
+ 	 *
+ 	 * @return the main
+ 	 */
+ 	public static final EasyRank getMain() {
 		 return main;
 	 }
 }
