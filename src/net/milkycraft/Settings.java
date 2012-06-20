6588930bb23795888d0bfdb9e96538a40e0a3300
Nick Porillo <milkywayz@mail.com>
2012-06-19 22:32:22 -0400

    javadoc

--- src/net/milkycraft/Settings.java
+++ src/net/milkycraft/Settings.java
@@ -1,7 +1,18 @@
 package net.milkycraft;
 
+/**
+ * The Class Settings.
+ */
 public class Settings extends ConfigLoader{
+	
+	/** The aroolp. */
 	public static boolean aroolp;
+	
+	/**
+	 * Instantiates a new settings.
+	 *
+	 * @param plugin the plugin
+	 */
 	public Settings(EasyRank plugin) {
 		super(plugin, "config.yml");
 		plugin = EasyRank.main;
@@ -9,6 +20,9 @@ public class Settings extends ConfigLoader{
 		saveIfNotExist();
 	}
 	
+	/* (non-Javadoc)
+	 * @see net.milkycraft.ConfigLoader#loadKeys()
+	 */
 	@Override
 	protected void loadKeys() {
 		aroolp = config.getBoolean("Easyrank.Allow.Ranking-Offline-Players");
